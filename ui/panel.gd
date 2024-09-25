b935a12452bfd2240bdccb643f6036746a375585
interloper <fatkiwii@proton.me>
2024-09-25 15:32:00 -0400

    HUD layout organizado

---
+++ ui/panel.gd
@@ -0,0 +1,11 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+#adicionar o crescimento na memoria baseado no uso de flags
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
