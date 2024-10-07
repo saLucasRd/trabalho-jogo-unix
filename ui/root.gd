7fac24398430b5993d8cec67d03f6232dd3d80b0
ludihan <65617704+ludihan@users.noreply.github.com>
2024-10-07 14:14:20 -0300

    polimento polimento polimento pol

--- ui/root.gd
+++ ui/root.gd
@@ -3,8 +3,8 @@ extends Control
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-			%DaemonView.grab_click_focus()
-			%DaemonView.grab_focus()
+			%FSView.grab_click_focus()
+			%FSView.grab_focus()
 			Global.on_fps_view = true
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)  # Capture the mouse
 
--- ui/terminal.gd
+++ ui/terminal.gd
@@ -1,72 +1,123 @@
 extends TextEdit
 
-var current_directory: String = "/"  # Starting directory
-var command_buffer: String = ""  # Buffer to hold the current command
-const MAX_LINES: int = 1000  # You can set this to a desired limit
-const CARET: String = "▋"
+const ROOT_DIR := "res://level/root"
+const MAX_LINES: int = 50
+const CARET: String = "█"
+var current_directory: String = "."
+var command_buffer: String = ""
+
+
 func _ready():
+	current_directory = ROOT_DIR
 	prepare_prompt()
 
+
 func _input(event):
-	if event is InputEventKey and event.pressed and !Global.on_fps_view:  # Ensure the event is a key event
+	if event is InputEventKey and event.pressed and !Global.on_fps_view:
 		match event.keycode:
 			KEY_ENTER:
 				execute_command(command_buffer)
 				prepare_prompt()
 			KEY_BACKSPACE:
 				if command_buffer.length() > 0:
-					command_buffer = command_buffer.substr(0, command_buffer.length() - 1)  # Remove last char from buffer
-					# Update the text to reflect the backspace without removing the caret
-					self.text = self.text.substr(0, self.text.length() - 2)  # Remove the char before the caret
-					self.text += CARET # Add the caret back
+					command_buffer = command_buffer.substr(0, command_buffer.length() - 1)
+					self.text = self.text.substr(0, self.text.length() - 2)
+					self.text += CARET
 			KEY_ESCAPE:
-				self.clear()  # Clear the terminal
+				self.clear()
 				prepare_prompt()
+			KEY_SPACE:
+				remove_last_caret()
+				command_buffer += " "
+				self.text += " " + CARET
 			_:
-				# Capture the character input
-				command_buffer += OS.get_keycode_string(event.keycode)  # Get the character from keycode
-				self.text = self.text.substr(0, self.text.length() - 1)  # Remove the old caret
-				self.text += OS.get_keycode_string(event.keycode) + CARET  # Display the text with caret
+				if event.keycode == clamp(event.keycode, 64, 90) or event.keycode == clamp(event.keycode, 48, 57):
+					update_caret(OS.get_keycode_string(event.keycode))
+				
+				if event.keycode == KEY_PERIOD:
+					update_caret(".")
+					
+				if Input.is_action_pressed("pipe"):
+					update_caret("|")
+				
+				if Input.is_action_just_pressed("underscore"):
+					update_caret("_")
 		limit_text()
 		scroll_to_bottom()
 
+
+func remove_last_caret():
+	self.text = self.text.substr(0, self.text.length() - 1)
+
+
+func update_caret(new_char: String):
+	command_buffer += new_char
+	remove_last_caret()
+	if new_char != " ":
+		self.text += new_char + CARET
+	if new_char == " ":
+		self.text += CARET
+
+
 func prepare_prompt():
-	command_buffer = ""  # Reset command buffer
-	print_prompt()  # Show the prompt again
+	command_buffer = ""
+	print_prompt()
+
 
 func print_prompt():
-	self.text += "\n$ " + CARET  # Display the prompt with caret
+	self.text += "\n$ " + CARET
+
 
 func limit_text():
-	var lines = self.text.split("\n")  # Split the text into lines
+	var lines = self.text.split("\n")
 	if lines.size() > MAX_LINES:
-		# Calculate the starting index for slicing
 		var start_index = lines.size() - MAX_LINES
 		self.text = "\n".join(lines.slice(start_index, lines.size()))
+
 
 func scroll_to_bottom():
 	self.scroll_vertical = self.get_line_count()
 
-func execute_command(command: String):
-	self.text = self.text.substr(0, self.text.length() - 1)  # Remove the old caret before executing the command
-	match command.strip_edges():
-		"":
-			pass
-		"ls":
-			self.text += "\nfile1.txt\nfile2.txt\nfolder1\n"
-		"cd folder1":
-			current_directory = "home/user/folder1"
-			self.text += "\nChanged directory to folder1"
-		"cd ..":
-			current_directory = "home/user"
-			self.text += "\nChanged directory to user"
-		_:
-			self.text += "\nCommand not found: " + command
-	limit_text()
-	scroll_to_bottom()
 
-# Called every time the timer times out to blink the caret
-func _on_caret_blink():
-	# Remove the old caret and add the new one
-	self.text = self.text.substr(0, self.text.length() - 1)  # Remove the last caret
-	self.text += CARET # Add the new caret at the end
+func execute_command(command: String):
+	remove_last_caret()
+	var pipe_args = command.strip_edges().split("|")
+	for i in range(pipe_args.size()):
+		var args = pipe_args[i].split(" ")
+		match args[0]:
+			"":
+				pass
+			"MAN":
+				self.text += "
+MAN [PROGRAMA]  MOSTRA AJUDA SOBRE COMANDOS
+LS  [DIRETORIO] MOSTRA CONTEUDO DE DIRETORIOS
+CD  DIRETORIO   MUDA DE DIRETORIO
+PWD             MOSTRA DIRETORIO ATUAL
+CAT ARQUIVO     MOSTRA / CONCATENA CONTEUDO DE ARQUIVOS"
+			"CD":
+				pass
+					
+			"PWD":
+				self.text += "\n" + current_directory
+			"LS":
+				pass
+			_:
+				self.text += "\nDSH: " + args[0] + ": COMMAND NOT FOUND"
+
+func bin_ls(arg: String, is_pipe: bool) -> bool:
+	var dir = DirAccess.open(arg)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				self.text += "\ndir: " + file_name
+			else:
+				self.text +=  "\nfile: " + file_name
+				file_name = dir.get_next()
+		return true
+	else:
+		return false
+
+func bin_cs(arg: String, is_pipe: bool):
+	pass
