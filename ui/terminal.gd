e93fdde41a2c29cc38edd8061b95d0a071987a9e
ludihan <65617704+ludihan@users.noreply.github.com>
2024-12-07 02:58:25 -0300

    ^C cancel terminal

--- ui/terminal.gd
+++ ui/terminal.gd
@@ -46,7 +46,21 @@ func scroll_to_bottom():
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
-		if !event.shift_pressed:
+		if Input.is_key_pressed(KEY_CTRL) and event.keycode == KEY_C:
+			update_caret("^C")
+			remove_last_caret()
+			self.text += "\n"
+
+			prepare_prompt()
+		if event.shift_pressed:
+			match event.keycode:
+				KEY_BACKSLASH:
+					update_caret("|")
+				KEY_MINUS:
+					update_caret("_")
+		if !event.shift_pressed and !Input.is_key_pressed(KEY_CTRL):
+			if event.keycode == clamp(event.keycode, 64, 90) or event.keycode == clamp(event.keycode, 48, 57):
+				update_caret(OS.get_keycode_string(event.keycode))
 			match event.keycode:
 				KEY_ENTER:
 					remove_last_caret()
@@ -61,9 +75,6 @@ func _on_gui_input(event: InputEvent) -> void:
 						command_buffer = command_buffer.substr(0, command_buffer.length() - 1)
 						self.text = self.text.substr(0, self.text.length() - 2)
 						self.text += CARET
-				KEY_ESCAPE:
-					self.clear()
-					prepare_prompt()
 				KEY_SPACE:
 					remove_last_caret()
 					command_buffer += " "
@@ -78,14 +89,6 @@ func _on_gui_input(event: InputEvent) -> void:
 					update_caret("/")
 				KEY_BACKSLASH:
 					update_caret("\\")
-		if event.shift_pressed:
-			match event.keycode:
-				KEY_BACKSLASH:
-					update_caret("|")
-				KEY_MINUS:
-					update_caret("_")
-		if event.keycode == clamp(event.keycode, 64, 90) or event.keycode == clamp(event.keycode, 48, 57):
-			update_caret(OS.get_keycode_string(event.keycode))
-
+		
 		limit_text()
 		scroll_to_bottom()
