72dc499097551e277eb8ac7a3cd4a61d7855025f
fatkiwi <88542169+saLucasRd@users.noreply.github.com>
2024-12-10 02:42:44 -0300

    pre alteracao no explorer

--- ui/explorer.gd
+++ ui/explorer.gd
@@ -4,6 +4,8 @@ extends Control
 
 func _ready() -> void:
 	$Panel2/VBoxContainer/Level1Container/Level1.pressed.connect(_on_level_1_pressed)
+	$Panel2/VBoxContainer/Level2Container/Level2.pressed.connect(_on_level_2_pressed)
+	$Panel2/VBoxContainer/Level3Container/Level3.pressed.connect(_on_level_3_pressed)
 
 func _on_level_1_pressed() -> void:
 	print("pressed")
@@ -11,3 +13,17 @@ func _on_level_1_pressed() -> void:
 		var new_scene = scene_to_instance.instantiate()
 		$Panel.add_child(new_scene)
 		#new_scene.position = Vector2(70, 0)
+
+
+func _on_level_2_pressed() -> void:
+	print("pressed")
+	if scene_to_instance:
+		var new_scene = scene_to_instance.instantiate()
+		$Panel.add_child(new_scene)
+
+
+func _on_level_3_pressed() -> void:
+	print("pressed")
+	if scene_to_instance:
+		var new_scene = scene_to_instance.instantiate()
+		$Panel.add_child(new_scene)
