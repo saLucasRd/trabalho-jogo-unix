92918bda3ae864148729ab609af1a2cb6923408a
fatkiwi <88542169+saLucasRd@users.noreply.github.com>
2024-12-10 01:30:26 -0300

    explorer levels, sla

--- ui/explorer.gd
+++ ui/explorer.gd
@@ -3,11 +3,11 @@ extends Control
 @export var scene_to_instance: PackedScene
 
 func _ready() -> void:
-	$Panel/GridContainer/Level1.pressed.connect(_on_level_1_pressed)
-
+	$Panel2/VBoxContainer/Level1Container/Level1.pressed.connect(_on_level_1_pressed)
 
 func _on_level_1_pressed() -> void:
 	print("pressed")
 	if scene_to_instance:
 		var new_scene = scene_to_instance.instantiate()
-		add_child(new_scene)
+		$Panel.add_child(new_scene)
+		#new_scene.position = Vector2(70, 0)
