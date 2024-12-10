00089ee125ef55f9f498d11b86f4091043fd137d
ludihan <65617704+ludihan@users.noreply.github.com>
2024-12-10 15:41:40 -0300

    levels

--- ui/level.gd
+++ ui/level.gd
@@ -4,16 +4,20 @@ extends Control
 @export var expected_answer: String = ""
 @export var level_path: String = ""
 
-@onready var question_label = $Label
-@onready var answer_input = $LineEdit
-@onready var submit_button = $Button
+
+@onready var question_label: RichTextLabel = $VBoxContainer2/RichTextLabel
+@onready var answer_input: LineEdit = $VBoxContainer2/HBoxContainer/LineEdit
+@onready var submit_button: Button = $VBoxContainer2/HBoxContainer/Button
+@onready var error: Label = $VBoxContainer2/VBoxContainer/Error
 
 func _ready():
 	# Set the question text
 	question_label.text = question
 
 
-func _on_submit_pressed():
+func _on_button_pressed() -> void:
+	if answer_input.text.is_empty():
+		error.text = "ERRO: Seu input está vazio."
 	var player_answer = answer_input.text.strip_edges().to_lower()
 	if player_answer == expected_answer.strip_edges().to_lower():
 		print("Correct! Moving to the next level.")
