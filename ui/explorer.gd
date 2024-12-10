4f6f0629fbea28e8104ca21061a7cb64b517359b
fatkiwi <88542169+saLucasRd@users.noreply.github.com>
2024-12-10 04:08:37 -0300

    file explorer

--- ui/explorer.gd
+++ ui/explorer.gd
@@ -1,29 +1,18 @@
 extends Control
 
-@export var scene_to_instance: PackedScene
-
-func _ready() -> void:
-	$Panel2/VBoxContainer/Level1Container/Level1.pressed.connect(_on_level_1_pressed)
-	$Panel2/VBoxContainer/Level2Container/Level2.pressed.connect(_on_level_2_pressed)
-	$Panel2/VBoxContainer/Level3Container/Level3.pressed.connect(_on_level_3_pressed)
+@export var levels: Array[Node]
 
 func _on_level_1_pressed() -> void:
-	print("pressed")
-	if scene_to_instance:
-		var new_scene = scene_to_instance.instantiate()
-		$Panel.add_child(new_scene)
-		#new_scene.position = Vector2(70, 0)
-
+	print("1")
+	_toggle_visibility($Panel/Level)
 
 func _on_level_2_pressed() -> void:
-	print("pressed")
-	if scene_to_instance:
-		var new_scene = scene_to_instance.instantiate()
-		$Panel.add_child(new_scene)
-
+	print("2")
+	_toggle_visibility($Panel/Level2)
 
 func _on_level_3_pressed() -> void:
-	print("pressed")
-	if scene_to_instance:
-		var new_scene = scene_to_instance.instantiate()
-		$Panel.add_child(new_scene)
+	print("3")
+	_toggle_visibility($Panel/Level3)
+
+func _toggle_visibility(node: Node) -> void:
+	node.visible = not node.visible
